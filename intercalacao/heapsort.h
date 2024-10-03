d83488aa90c3538d0aacdfd20308fd3ac57698b8
Davi Cardoso <davideoliveira2003@gmail.com>
2024-10-02 23:33:32 -0300

    Tentativa 2

--- intercalacao/heapsort.h
+++ intercalacao/heapsort.h
@@ -4,9 +4,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void heapSort(int *vet, int tam);
-void criaHeap(int *vet, int i, int finalVetor);
-void EscreverMenorElemento(int *vet, int tam, FILE *arquivo_saida);
+typedef struct {
+    int CodigoCliente;
+    char nome[50];
+    char DataNascimento[20];
+}CLIENTE;
+
+typedef struct {
+    CLIENTE cliente;
+    int indiceParticao;
+}NO;
+
+typedef struct {
+    NO *nos;
+    int tamanho;
+}HEAP;
+
+void heapSort(HEAP *heap, NO nos);
+void criaHeap(HEAP *heap, int i);
+NO EscreverMenorElemento(HEAP *heap);
+void trocaNOS (NO *heapA, NO *heapB);
+void fechaArquivos(int tamanhoArquivo, FILE *arquivos[]);
 
 
 #endif
